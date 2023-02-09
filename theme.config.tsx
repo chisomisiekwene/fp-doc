7b75b0223d8b682131d7cf0235bb2f451514884d
chisomisiekwene <chisomisiekwene57@gmail.com>
2023-02-09 09:06:11 +0100

    added all files

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Fountainpay</span>,
+  project: {
+    link: "https://github.com/shuding/nextra-docs-template",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "Fountainpay Doc",
+  },
+};
+
+export default config;
